8333f03536abd54d1fdd96aae72b48c5c9c3750e
Adi Jaya <adijaya.djay47@gmail.com>
2025-06-14 02:07:52 +0700

    Add database context class

---
+++ Data/DevelopmentDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspDotNetCoreMvcApp.Data;
+
+public partial class DevelopmentDbContext : DbContext
+{
+    public DevelopmentDbContext()
+    {
+    }
+
+    public DevelopmentDbContext(DbContextOptions<DevelopmentDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Name=SqliteConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
